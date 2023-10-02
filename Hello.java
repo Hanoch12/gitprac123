a0835206e188af86b88b36b91894c653c96cfb05
Thummanapally <hanokteja75@gmail.com>
2023-10-02 11:43:09 +0000

    made changes bro to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
-class Hello
+Class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“hello once again someone has made changes to check”);
+System.out.println(“hello once again someone has made changes to check whether tracking or not”);
 }
 }
 
