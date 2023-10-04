7d59758f5a1288efb25a60b63cf85fcae151691a
Hanoch12 <115057156+Hanoch12@users.noreply.github.com>
2023-10-04 19:47:04 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ Class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“hello from feature-192 changes made in remote ”);
+System.out.println(“hello from feature-192 changes made in remote not locally ”);
 }
 }
 
