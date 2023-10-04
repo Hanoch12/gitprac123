154154b8ccc0662511ee24575eb8364242857267
Thummanapally <hanokteja75@gmail.com>
2023-10-04 17:48:26 +0000

    commiting from local

--- Hello.java
+++ Hello.java
@@ -2,7 +2,11 @@ Class Hello
 {
 Public static void main(String args[])
 {
+<<<<<<< Updated upstream
 System.out.println(“hello from master”);
+=======
+System.out.println(“hello from master to check stash check”);
+>>>>>>> Stashed changes
 }
 }
 
