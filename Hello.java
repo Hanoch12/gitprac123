9057243976c2a66245b14a84637b8523dd03a13e
Thummanapally <hanokteja75@gmail.com>
2023-10-04 17:52:10 +0000

    resolved

--- Hello.java
+++ Hello.java
@@ -2,11 +2,8 @@ Class Hello
 {
 Public static void main(String args[])
 {
-<<<<<<< Updated upstream
-System.out.println(“hello from master”);
-=======
 System.out.println(“hello from master to check stash check”);
->>>>>>> Stashed changes
+
 }
 }
 
